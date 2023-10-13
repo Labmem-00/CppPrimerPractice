95b772f6d681247d3345d6991af56e6023980bb3
Labmem.No.004 <727460137@qq.com>
2023-10-13 16:11:20 +0800

    new:

---
+++ chapter4/4.25.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main()
+{
+    ulong u11=3,u12=7;
+    int i=ulong((~'q')<<6);
+    cout<<bitset<8>(i)<<endl;//输出8位二进制数。
+    cout<<(u11&u12);//3
+    cout<<(u11|u12);//7
+    //&&，||输出都为1
+    cout<<sizeof(int);
+    return 0;
+} 
